ce026cbcc8ccb217e112c6f31046cc3ddfe011ee
yash15-exe <yashraikar153@gmail.com>
2024-02-06 21:00:00 +0530

    small changes in idgen

--- utilities/idGenerator.js
+++ utilities/idGenerator.js
@@ -1,7 +1,7 @@
-export default function idGenerator(){
+export default function idGenerator(length){
     const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     let id ="";
-    for (i = 1; i < 11; i++) {
+    for (i = 1; i <= length; i++) {
      id += alpha[Math.round(Math.random() * 36)];
     }
     return id;
