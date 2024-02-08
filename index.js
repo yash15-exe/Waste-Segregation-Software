aa9ddfa7e8b17db477b727b8ef066286193ed4fc
yash15-exe <yashraikar153@gmail.com>
2024-02-08 22:55:32 +0530

    Login functionality established

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-import express from "express";
-import dotenv from "dotenv";
+
+const express = require("express");
+const dotenv = require("dotenv");
+const ejs = require("ejs");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+
 dotenv.config();
 
-
-
-const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 let userIsAuthenticated = false;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
 const isAuthenticated = (req, res, next) => {
   const { password } = req.body;
@@ -23,21 +36,14 @@ const isAuthenticated = (req, res, next) => {
   }
 };
 
-
-
 app.get("/dashboard", isAuthenticated, (req, res) => {
-
   userIsAuthenticated = false;
-  res.send("Welcome to your dashboard!");
+  res.render("login1");
 });
 
-
-
-app.get("/login", (req, res) => {
+app.post("/login", (req, res) => {
   const { password } = req.body;
-  console.log(password);
   let loginSuccessful = false;
-  console.log(process.env.PASSWORD);
   if (password === process.env.PASSWORD) {
     userIsAuthenticated = true;
 
@@ -45,16 +51,12 @@ app.get("/login", (req, res) => {
   }
   if (loginSuccessful) {
     userIsAuthenticated = true;
-    res.redirect("/dashboard");
+    res.status(200).json({ success: true, message: "Login successful" });
   } else {
-
-    res.send("Login failed. Try again.");
+    res.status(401).send("Login failed. Try again.");
   }
 });
 
-
-
-
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server listening on Port ${process.env.PORT}`);
 });
